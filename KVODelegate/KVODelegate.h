398fba02947ddaedfab56187762b1cc734198f5d
ThatsJustCheesy <justcheesy@outlook.com>
2017-03-15 14:47:51 -0400

    Initial Commit

---
+++ KVODelegate/KVODelegate.h
@@ -0,0 +1,19 @@
+//
+//  KVODelegate.h
+//  KVODelegate
+//
+//  Created by Ian Gregory on 15-03-2017.
+//  Copyright © 2017 ThatsJustCheesy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for KVODelegate.
+FOUNDATION_EXPORT double KVODelegateVersionNumber;
+
+//! Project version string for KVODelegate.
+FOUNDATION_EXPORT const unsigned char KVODelegateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KVODelegate/PublicHeader.h>
+
+

2a77fc25c4ecdc501547f410fdd47b8194df23f1
ThatsJustCheesy <justcheesy@outlook.com>
2017-03-16 18:09:49 -0400

    Re-add test targets

---
+++ KVODelegateObjCTests/ObjCTests.m
@@ -0,0 +1,76 @@
+//
+//  KVODelegateTests.m
+//  KVODelegateTests
+//
+//  Created by Ian Gregory on 15-03-2017.
+//  Copyright © 2017 ThatsJustCheesy. All rights reserved.
+//
+
+@import XCTest;
+@import KVODelegate;
+
+@interface Person : NSObject
+@property NSString *name, *address, *postalCode;
+@property NSArray<NSNumber*> *creditCardNumbers;
+@end
+
+@implementation Person
+@end
+
+@interface KVODelegateTests : XCTestCase
+@property KVOObservationDelegate *kvoDelegate;
+@end
+
+@implementation KVODelegateTests
+
+- (void)setUp {
+    [super setUp];
+    self.kvoDelegate = [KVOObservationDelegate new];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)testBasicObserving {
+    Person *a = [Person new];
+    a.name = @"Bob";
+    a.address = @"123 Happy Street";
+    
+    dispatch_semaphore_t sema = dispatch_semaphore_create(0);
+    dispatch_time_t timeout = dispatch_time(DISPATCH_TIME_NOW, 1000*NSEC_PER_MSEC);
+    long signalReceived;
+    
+    [self.kvoDelegate startObservingKeyPath:@"postalCode" on:a usingBlock:^{
+        dispatch_semaphore_signal(sema);
+    }];
+    a.postalCode = @"A1B 2C3";
+    signalReceived = dispatch_semaphore_wait(sema, timeout);
+    XCTAssert(signalReceived == 0);
+    
+    [self.kvoDelegate stopObservingKeyPath:@"postalCode" on:a];
+    a.postalCode = @"A1B 2C4";
+    signalReceived = dispatch_semaphore_wait(sema, timeout);
+    XCTAssert(signalReceived != 0);
+    
+    [self.kvoDelegate startObservingKeyPath:@"address" on:a usingNewOldBlock:^(id newValue, id oldValue) {
+        XCTAssert([a.address isEqual:newValue]);
+        if ([newValue containsString:[oldValue substringFromIndex:4]]) a.postalCode = @"A1B 2D4";
+        else a.postalCode = @"X7Y 8Z9";
+        dispatch_semaphore_signal(sema);
+    } options:NSKeyValueObservingOptionNew|NSKeyValueObservingOptionOld];
+    a.address = @"456 Happy Street";
+    signalReceived = dispatch_semaphore_wait(sema, timeout);
+    XCTAssert(signalReceived == 0);
+    XCTAssert([a.postalCode isEqualToString:@"A1B 2D4"]);
+    a.address = @"1 Cherry Lane";
+    signalReceived = dispatch_semaphore_wait(sema, timeout);
+    XCTAssert(signalReceived == 0);
+    XCTAssert([a.postalCode isEqualToString:@"X7Y 8Z9"]);
+    
+    [self.kvoDelegate stopObservingKeyPath:@"address" on:a];
+    
+    [self.kvoDelegate stopObservingAllKeyPathsOn:a];
+}
+
+@end
---
+++ KVODelegateSwiftTests/SwiftTests.swift
@@ -0,0 +1,75 @@
+//
+//  SwiftTests.swift
+//  KVODelegate
+//
+//  Created by Ian Gregory on 15-03-2017.
+//  Copyright © 2017 ThatsJustCheesy. All rights reserved.
+//
+
+import XCTest
+import KVODelegate
+
+class Person: NSObject {
+    dynamic var name = "", address = "", postalCode = ""
+    dynamic var creditCardNumbers: [Int] = []
+}
+
+class KVODelegateTests: XCTestCase {
+    
+    var kvoDelegate = KVOObservationDelegate()
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testBasicObserving() {
+        let a = Person()
+        a.name = "Bob"
+        a.address = "123 Happy Street"
+        
+        let sema = DispatchSemaphore(value: 0)
+        let timeout = DispatchTime.now() + DispatchTimeInterval.milliseconds(1000)
+        var signalReceived: DispatchTimeoutResult
+        
+        kvoDelegate.startObserving(keyPath: "postalCode", on: a, using: {
+            sema.signal()
+        })
+        a.postalCode = "A1B 2C3"
+        signalReceived = sema.wait(timeout: timeout)
+        XCTAssert(signalReceived == .success)
+        
+        kvoDelegate.stopObserving(keyPath: "postalCode", on: a)
+        a.postalCode = "A1B 2C4"
+        signalReceived = sema.wait(timeout: timeout)
+        XCTAssert(signalReceived == .timedOut)
+        
+        kvoDelegate.startObserving(keyPath: "address", on: a, using: { (new, old) in
+            XCTAssert(a.address.isEqual(new))
+            if let new = new as? String, let old = old as? String {
+                if new.contains(old.substring(from: old.index(old.startIndex, offsetBy: 4))) {
+                    a.postalCode = "A1B 2D4"
+                } else {
+                    a.postalCode = "X7Y 8Z9"
+                }
+            }
+            sema.signal()
+        }, options: [.new, .old])
+        a.address = "456 Happy Street"
+        signalReceived = sema.wait(timeout: timeout)
+        XCTAssert(signalReceived == .success)
+        XCTAssert(a.postalCode == "A1B 2D4")
+        a.address = "1 Cherry Lane"
+        signalReceived = sema.wait(timeout: timeout)
+        XCTAssert(signalReceived == .success)
+        XCTAssert(a.postalCode == "X7Y 8Z9")
+        
+        kvoDelegate.stopObserving(keyPath: "address", on: a)
+        
+        kvoDelegate.stopObserving(allKeyPathsOn: a)
+    }
+    
+}
